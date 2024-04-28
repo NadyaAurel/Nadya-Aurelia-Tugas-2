2f4d46dbc842244b92ed405748e6b45086d06549
NadyaAurel <41522010273@student.mercubuana.ac.id>
2024-04-28 21:55:29 +0700

    Update terbaru

--- app/src/main/java/com/example/tugas2login/activity2register.java
+++ app/src/main/java/com/example/tugas2login/activity2register.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
@@ -20,6 +21,11 @@ public class activity2register extends AppCompatActivity {
         setContentView(R.layout.activity_activity2register);
 
         pindahlogin = findViewById(R.id.btnlogin);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
         pindahlogin.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/tugas2login/tbtampilanawal.java
+++ app/src/main/java/com/example/tugas2login/tbtampilanawal.java
@@ -1,9 +1,6 @@
 package com.example.tugas2login;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,28 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class login3tugas extends AppCompatActivity {
-
-    Button masukuser;
+public class tbtampilanawal extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login3tugas);
-        masukuser = findViewById(R.id.btnbiru);
-
+        setContentView(R.layout.activity_tbtampilanawal);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-        masukuser.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent pindahkeuser = new Intent(login3tugas.this, tbtampilanawal.class);
-                startActivity(pindahkeuser);
-            }
         });
     }
 }
